8f3f582ed2125d2096029d497c7fecbe691b5b8d
Fernanda Andrade <119636766+fe222004@users.noreply.github.com>
2024-06-05 19:39:00 -0500

    Update user.service.ts

--- src/user.service.ts
+++ src/user.service.ts
@@ -2,7 +2,7 @@
 export class UserServices{
 constructor(@Inyect('USER_REPOSITORY') private redonly userRepository :Repository<UserEntity>){}
 
-  findAll(){
-    const users = this.userRepository.find()
+  async findAll(){
+    const users = await this.userRepository.find()
   }
 }
