9940c6a1ff59bdb68e07650bab60f77e2af67d40
Fernanda Andrade <119636766+fe222004@users.noreply.github.com>
2024-06-05 20:04:05 -0500

    Update user.service.ts

--- src/user.service.ts
+++ src/user.service.ts
@@ -6,8 +6,8 @@ constructor(@Inyect('USER_REPOSITORY') private redonly userRepository :Repositor
     const users = await this.userRepository.find()
   }
 
-  async findOneUser(){
-    const user = await this.userRepository.find()
+  async findOneUser(id,name,lastname,email,password){
+    const user = await this.userRepository.find(id,name,lastname,email,password)
   }
 
   //el paginador se hace aqui 
