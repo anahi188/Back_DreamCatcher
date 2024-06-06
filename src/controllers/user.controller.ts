ea859ded329d329c4569a01ef74ad3303d412f90
Fernanda Andrade <119636766+fe222004@users.noreply.github.com>
2024-06-05 19:33:19 -0500

    Update user.controller.ts

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,1 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from '../app.service';
 
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getHello(): string {
+    return this.userService.findAll ();
+  }
+}
