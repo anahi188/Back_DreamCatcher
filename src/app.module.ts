550d6c35e1fd44e220756682be9abfe8b4fe2dfe
Keoc1 <co298457@gmail.com>
2024-06-15 06:05:19 +0200

    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-
-import { AppController } from './controllers/app.controller';
 import { StateRevisorController } from './controllers/state-revisor/state-revisor.controller';
 import { ExplorerService } from './services/explorer/explorer.service';
 import { RevisorService } from './services/revisor/revisor.service';
 import { postProviders } from './providers/post.providers';
 import { stateRevisorProviders } from './providers/stateRevisor.providers';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [DatabaseModule],
 
-  controllers: [AppController, StateRevisorController,],
+  controllers: [ StateRevisorController,],
   providers: [AppService, ExplorerService, RevisorService,  ...postProviders,  ...stateRevisorProviders, ],
 
 })
