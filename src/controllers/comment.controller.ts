3bb6f8db2d29d0efe44d45a40a03e47c435adc8f
Joss Balarezo <151807265+jossy0203@users.noreply.github.com>
2024-06-15 11:01:29 -0500

    rutas

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { CommentService } from "src/services/comment.service";
 
-@Controller('coment')
+@Controller('coments')
 export class CommentController{
 
     constructor(private readonly commentService: CommentService){
