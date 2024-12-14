18dec9a39fee23bb9b5698628785f6f6d3533862
Fernanda Andrade <119636766+fe222004@users.noreply.github.com>
2024-12-13 20:57:46 -0500

    post

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import * as express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { existsSync, mkdirSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Cast a NestExpressApplication
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const uploadsPath = join(__dirname, '..', 'uploads');
 
-  
+  // Sirve archivos estáticos desde la carpeta uploads/posts
+  app.useStaticAssets(join(__dirname, '..', 'uploads', 'posts'), {
+    prefix: '/uploads/posts/', // La URL será http://localhost:3009/uploads/posts/
+  });
+
   // Verificar y crear las carpetas si no existen
   if (!existsSync(uploadsPath)) {
     mkdirSync(uploadsPath, { recursive: true });
   }
 
+  // Configuración de CORS
   app.enableCors({
     origin: 'http://localhost:4200', // Permite solo este origen
     methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
     credentials: true, // Si manejas cookies o autenticación
   });
 
+  // Escuchar en el puerto 3009
   await app.listen(3009);
 }
+
 bootstrap();
